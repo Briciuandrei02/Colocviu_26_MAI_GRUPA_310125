0d15781ff03c59d361fdfc93275acd6caf505d9a
Briciuandrei02 <127474521+Briciuandrei02@users.noreply.github.com>
2023-05-26 08:46:37 +0300

    ZborDrone.java

--- untitled/src/ZborDrone/ActiuneAdauga.java
+++ untitled/src/ZborDrone/ActiuneAdauga.java
@@ -1,6 +1,32 @@
-package ZborDrone;
+import java.io.Serializable;
 
-import java.awt.event.ActionListener;
+// Clasa ZborDrone care implementeaza interfata Serializable pentru a putea fi serializata
+class ZborDrone implements Serializable {
+    private String identificatorUnic;//modelul dronei
+    private double razaMaximaDeZbor;//acel maximum_flight_range
+    private String tipUnic;//tipul dronei
 
-public class ActiuneAdauga implements ActionListener {
+    public ZborDrone(String identificatorUnic, double razaMaximaDeZbor, String tipUnic) {
+        this.identificatorUnic = identificatorUnic;
+        this.razaMaximaDeZbor = razaMaximaDeZbor;
+        this.tipUnic = tipUnic;
+    }
+
+    // getteri si setteri
+    public String getIdentificatorUnic() {
+        return identificatorUnic;
+    }
+
+    public double getRazaMaximaDeZbor() {
+        return razaMaximaDeZbor;
+    }
+
+    public String getTipUnic() {
+        return tipUnic;
+    }
+
+    @Override
+    public String toString() {
+        return "Model: " + identificatorUnic + ", Raza Maxima de Zbor: " + razaMaximaDeZbor + ", Tip: " + tipUnic;
+    }
 }
